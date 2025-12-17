46a430dbd39c7c21370cffcc0454544ab307cbfb
Richmond Boakye <richmondboakye0017@gmail.com>
2025-12-17 15:48:47 +0100

    Have registered the dependency injection in the program file with loose coupling to the mock data and tested it with Postman and everything is working perfectly.

--- BookCollectionlAPI/Controllers/BooksController.cs
+++ BookCollectionlAPI/Controllers/BooksController.cs
@@ -7,26 +7,35 @@ namespace BookCollectionAPI.Controllers
 
     // Route will be: api/books
     // Using [controller] keeps the route in sync if the controller name changes
-    [Route ("api/[controller]")]
+    // [Route ("api/[controller]")]
     [ApiController]
+    [Route ("api/books")]
     public class BooksController: ControllerBase
     {
+        //private readonly MockBookRepo _bookRepo = new MockBookRepo();
+        private readonly IBookCollectionRepo _repository;
 
-        private readonly MockBookRepo _bookRepo = new MockBookRepo();
+        public BooksController(IBookCollectionRepo repository)
+        {
+            _repository = repository;
+        }
+
+       
         //GET api/books/
         [HttpGet]
         public ActionResult <IEnumerable<Book>> GetAllBooks()
         {
-            var bookItems =_bookRepo.GetBooks();
+            var bookItems = _repository.GetBooks();
 
             return Ok(bookItems);
+           
         }
 
         //GET api/books/{id}
         [HttpGet("{id}")]
         public ActionResult<Book> GetBookById(int id)
         {
-            var bookItem=_bookRepo.GetBookById(id);
+            var bookItem= _repository.GetBookById(id);
             return Ok(bookItem);
         }
     }
--- BookCollectionlAPI/Program.cs
+++ BookCollectionlAPI/Program.cs
@@ -1,8 +1,13 @@
+using BookCollectionAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<IBookCollectionRepo, MockBookRepo>();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
