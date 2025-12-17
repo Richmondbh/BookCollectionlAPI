2597b31e2f8f91f0fce7039efb2c466fe43ddff6
Richmond Boakye <richmondboakye0017@gmail.com>
2025-12-17 09:57:15 +0100

    Implemented the codes in the controller to test it with postman

--- BookCollectionlAPI/Controllers/BooksController.cs
+++ BookCollectionlAPI/Controllers/BooksController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookCollectionAPI.Data;
+using BookCollectionAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookCollectionAPI.Controllers
 {
@@ -10,5 +12,22 @@ namespace BookCollectionAPI.Controllers
     public class BooksController: ControllerBase
     {
 
+        private readonly MockBookRepo _bookRepo = new MockBookRepo();
+        //GET api/books/
+        [HttpGet]
+        public ActionResult <IEnumerable<Book>> GetAllBooks()
+        {
+            var bookItems =_bookRepo.GetBooks();
+
+            return Ok(bookItems);
+        }
+
+        //GET api/books/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBookById(int id)
+        {
+            var bookItem=_bookRepo.GetBookById(id);
+            return Ok(bookItem);
+        }
     }
 }
