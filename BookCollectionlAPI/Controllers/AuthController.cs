8e1b23eb2fcdcd7ddaa0568fd8acb69a55a1eac0
Richmond Boakye <richmondboakye0017@gmail.com>
2025-12-21 17:42:05 +0100

    I have started configuring the user authentications

---
+++ BookCollectionlAPI/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using BookCollectionAPI.Dtos;
+using BookCollectionAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookCollectionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        // POST: api/auth/register
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+           
+        }
+
+    }
+}
